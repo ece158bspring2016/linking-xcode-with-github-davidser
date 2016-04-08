0c09288ac9e495d68db4a34e22448e6c47735538
David Serrano <davidserrano@davids-macbook-pro-3.local>
2016-04-08 12:18:56 -0700

    Added print statement

--- GitHubLink/ViewController.swift
+++ GitHubLink/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("I can't wait to push this project to GitHub")
+        print("Did this push?")
     }
 
     override func didReceiveMemoryWarning() {
